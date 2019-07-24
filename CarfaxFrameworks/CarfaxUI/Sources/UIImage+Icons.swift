c6de168ae3a83d4d3ad1e56683a73169e3f8df3d
JGVB <jvanbev1@gmail.com>
2019-07-24 15:24:59 -0400

    add image assets - phone icon

---
+++ CarfaxFrameworks/CarfaxUI/Sources/UIImage+Icons.swift
@@ -0,0 +1,19 @@
+///
+/// This file is created at build time from scripts/image_assets.swift
+/// Do not modify directly.
+///
+
+import UIKit
+
+private class Container {}
+
+public extension UIImage {
+
+    private convenience init(iconNamed: String) {
+        self.init(named: iconNamed, in: Bundle(for: Container.self), compatibleWith: nil)!
+    }
+
+    static var icon_phone: UIImage {
+        return UIImage(iconNamed: "icon-phone")
+    }
+}
